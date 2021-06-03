3b1dc13d3ac04ab0fb6e74bc483350e2c5b456b2
SzMatthew <szabomati@gmail.com>
2021-06-03 18:48:17 +0200

    Add Project route and modal

--- routes/projects.js
+++ routes/projects.js
@@ -1,9 +1,30 @@
 const express = require('express');
-
+const Project = require('../models/Project');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Projects');
+// GET
+router.get('/', async (req, res) => {
+    try {
+        const project = await Project.find();
+        res.json(project);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+// POST
+router.post('/', async (req, res) => {
+    const project = new Project({
+        title: req.body.title
+    });
+
+    project.save()
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.json({ message: err });
+        })
 });
 
 module.exports = router;
